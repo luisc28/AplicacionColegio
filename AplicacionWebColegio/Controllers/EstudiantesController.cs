8e9dc5f1c9a8bd86224b85e80e2c45d4b529de5e
luisc28 <devluisc@gmail.com>
2022-06-08 16:30:20 -0500

    Edicion de prueba.

---
+++ AplicacionWebColegio/Controllers/EstudiantesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AplicacionWebColegio.Models;
+
+namespace AplicacionWebColegio.Controllers
+{
+    public class EstudiantesController : Controller
+    {
+        // GET: Estudiantes
+        public ActionResult Index()
+        {
+            List<EstudianteCLS> listaEstudiante = null;
+            using (var bd = new BDCOLEGIOBOTOGAEntities())
+            {
+                listaEstudiante = (from estudiante in bd.Estudiantes
+                                  where estudiante.BHABILITADO == 1
+                                  select new EstudianteCLS
+                                  {
+                                      iidestudiante = estudiante.IIDESTUDIANTES,
+                                      nombre = estudiante.NOMBRE,
+                                      apellido = estudiante.APELLIDO,
+                                      curso = estudiante.CURSO,
+                                      email = estudiante.EMAIL
+                                  }).ToList();
+            }
+            return View(listaEstudiante);
+        }
+    }
+}
---
+++ AplicacionWebColegio/Models/ProfesorCLS.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AplicacionWebColegio.Models
+{
+    public class ProfesorCLS
+    {
+        [Display(Name ="ID Profesor")]
+        public int iidprofesores { get; set; }
+        [Display(Name = "Nombre Profesor")]
+        public string nombre { get; set; }
+        [Display(Name = "Apellido Profesor")]
+        public string apellido { get; set; }
+        [Display(Name = "Materia")]
+        public string materia { get; set; }
+        [Display(Name = "Email")]
+        public string email { get; set; }
+        public int bhabilitado { get; set; }
+    }
+}
