60319bbf798a1a46a121e79c8c184a498f0fc69a
luisc28 <devluisc@gmail.com>
2022-06-08 16:30:20 -0500

    Agregar archivos de proyecto.

---
+++ AplicacionWebColegio/Controllers/ProfesoresController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AplicacionWebColegio.Models;
+
+namespace AplicacionWebColegio.Controllers
+{
+    public class ProfesoresController : Controller
+    {
+        // GET: Profesores
+        public ActionResult Index()
+        {
+            List<ProfesorCLS> listaProfesor = null;
+            using (var bd= new BDCOLEGIOBOTOGAEntities())
+            {
+                listaProfesor = (from profesor in bd.Profesores
+                                 where profesor.BHABILITADO == 1
+                                 select new ProfesorCLS
+                                 {
+                                     iidprofesores = profesor.IIDPROFESORES,
+                                     nombre = profesor.NOMBRE,
+                                     apellido = profesor.APELLIDO,
+                                     materia = profesor.MATERIA,
+                                     email = profesor.EMAIL
+                                 }).ToList();
+            }
+            return View(listaProfesor);
+        }
+    }
+}
---
+++ AplicacionWebColegio/Models/EstudianteCLS.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AplicacionWebColegio.Models
+{
+    public class EstudianteCLS
+    {
+        [Display(Name ="ID Estudiante")]
+        public int iidestudiante { get; set; }
+        [Display(Name = "Nombre Estudiante")]
+        public string nombre { get; set; }
+        [Display(Name = "Apellido Estudiante")]
+        public string apellido { get; set; }
+        [Display(Name = "Curso")]
+        public string curso { get; set; }
+        [Display(Name = "Email")]
+        public string email { get; set; }
+        public int bhabilitado { get; set; }
+
+    }
+}
